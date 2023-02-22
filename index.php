3ba0f65e50acd1438011e0ef47ea7f21529cbe50
Pawel Truszkowski <pawel.truszkowski14@gmail.com>
2023-02-22 18:00:16 +0100

    Added new file db_config

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
-    echo "Hello World!";
-
+    
+    #echo "Hello World!";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List To-Do App</title>
 </head>
+
 <body>
+    <h1>List To-Do App</h1>
     
+
 </body>
+
 </html>
