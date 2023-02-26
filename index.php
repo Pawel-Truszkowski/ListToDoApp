fe37db10412d57595d4841525dff102ef3f76e76
Pawel Truszkowski <pawel.truszkowski14@gmail.com>
2023-02-26 13:21:45 +0100

    Style finished

--- index.php
+++ index.php
@@ -27,6 +27,15 @@ $listQuery = $db->prepare("SELECT * FROM tasks");
 $listQuery->execute();
 $list = $listQuery->fetchAll(PDO::FETCH_ASSOC);
 
+#Update tasków
+
+if (isset($_GET['update'])) {
+    $id = $_GET['update'];
+    $updateQuery = $db->prepare("UPDATE tasks SET task = :task WHERE id = :id");
+    $updateQuery->execute(['task' => $task, 'id' => $id]);
+    header('Location: index.php');
+}
+
 #Usuwamy zadanie
 if (isset($_GET['delete_task'])) {
     $id = $_GET['delete_task'];
@@ -47,51 +56,73 @@ if (isset($_GET['delete_task'])) {
     <title>List To-Do App</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 
 <body>
     <header class="container">
         <div class="row">
-            <h1 class="col-md-6">List To-Do App</h1>
+            <h1 class="col-md-12 text-left">List To-Do App</h1>
         </div>
     </header>
-    <div class="container">
-        <form method="post" action="index.php">
-            <label for="fname">Save your task:</label>
-            <input type="text" name="task" placeholder="This field is required" required>
-            <?php
-            if (isset($error)) ?>
-            <p><?= $error; ?> </p>
-            <button type="submit" name="submit">Submit</button>
-        </form>
-        <h2>Current Tasks</h2>
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nr</th>
-                        <th>Task</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <form method="post" action="index.php">
+                    <p><label for="fname">Save your task:</label></p>
+                    <textarea id="w3review" name="task" rows="3" cols="50" required></textarea>
+                    <br>
                     <?php
-                    $count = 1;
-                    foreach ($list as $task) {
-                    ?>
-                        <tr>
-                            <td class="nr"><?= $count ?></td>
-                            <td class="task"><?= $task['task'] ?></td>
-                            <td class="delete">
-                                <a href="index.php?delete_task=<?= $task['id']; ?>">X</a>
-                            </td>
-                        </tr>
-                    <?php $count++;
-                    } ?>
-                </tbody>
-            </table>
+                    if (isset($error)) ?>
+                    <p><?= $error; ?> </p>
+                    <button type="submit" name="submit">Submit</button>
+                </form>
+            </div>
         </div>
-    </div>
+    </section>
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Current Tasks</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <table class="table">
+                    <thead>
+                        <tr class="table">
+                            <th>Nr</th>
+                            <th>Task</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $count = 1;
+                        foreach ($list as $task) {
+                        ?>
+                            <tr class="table">
+                                <td class="nr"><?= $count ?></td>
+                                <td class="task"><?= $task['task'] ?></td>
+                                <td class="update">
+                                    <a href="index.php?update=<?= $task['id']; ?>">
+                                        <span class="material-symbols-outlined">
+                                            update
+                                        </span>
+                                    </a>
+                                </td>
+                                <td class="delete">
+                                    <a href="index.php?delete_task=<?= $task['id']; ?>">X</a>
+                                </td>
+                            </tr>
+                        <?php $count++;
+                        } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
